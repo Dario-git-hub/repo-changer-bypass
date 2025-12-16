65e3b4e2ad627e495b95caef288eac76e0a41f51
Dario-git-hub <dariomiguelnavarro822@gmail.com>
2025-12-16 09:25:00 +0100

    quick script to bypass restrictions &install spotify

---
+++ main.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo 'deb http://debian.rutgers.edu/debian/ bullseye main contrib non-free
+deb http://debian.rutgers.edu/debian/ bullseye-updates main
+deb http://debian.rutgers.edu/security bullseye-security main contrib non-free' | sudo tee /etc/apt/sources.list
+
+curl -sS 'https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg' | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo 'deb https://repository.spotify.com stable non-free' | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update
+sudo apt-get install spotify-client
