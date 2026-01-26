9b5b37663fd550ac2588dd5f5899a3a50c6f0952
Dario-git-hub <dariomiguelnavarro822@gmail.com>
2026-01-26 23:05:52 +0100

    snap-store

--- main.sh
+++ main.sh
@@ -7,4 +7,6 @@ deb http://debian.rutgers.edu/security bullseye-security main contrib non-free' 
 curl -sS 'https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg' | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo 'deb https://repository.spotify.com stable non-free' | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
-sudo apt-get install -y spotify-client
+sudo apt-get install -y spotify-client snapd
+sudo systemd enable --now snapd 
+sudo snap intall snap-store
