ea51d1fc3edd3797ad94c6fac44c7a35321f56c3
Dario-git-hub <dariomiguelnavarro822@gmail.com>
2026-03-24 17:34:38 +0100

    update spotify pubkey

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@
 echo 'deb https://cloudfront.debian.net/debian/ bookworm main contrib non-free
 deb https://cloudfront.debian.net/debian/ bookworm-updates main' | sudo tee /etc/apt/sources.list
 
-curl -sS 'https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg' | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+curl -sS https://download.spotify.com/debian/pubkey_5384CE82BA52C83A.asc | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo 'deb https://repository.spotify.com stable non-free' | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
-sudo apt-get install -y spotify-client
+sudo apt-get install -y spotify-client 
