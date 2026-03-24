5d582618dc2ac760d391852060fac847c97bd716
Dario-git-hub <dariomiguelnavarro822@gmail.com>
2026-03-24 12:59:06 +0100

    Update Debian sources and modify package installation
    Updated Debian sources to use CloudFront and changed the installation method for snapd and snap-store.

--- main.sh
+++ main.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-echo 'deb http://debian.rutgers.edu/debian/ bullseye main contrib non-free
-deb http://debian.rutgers.edu/debian/ bullseye-updates main
-deb http://debian.rutgers.edu/security bullseye-security main contrib non-free' | sudo tee /etc/apt/sources.list
+echo 'deb https://cloudfront.debian.net/debian/ bookworm main contrib non-free
+deb https://cloudfront.debian.net/debian/ bookworm-updates main' | sudo tee /etc/apt/sources.list
 
 curl -sS 'https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg' | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo 'deb https://repository.spotify.com stable non-free' | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
-sudo apt-get install -y spotify-client snapd
-sudo systemctl enable --now snapd 
-sudo snap install snap-store
+sudo apt-get install -y spotify-client
