72d731ef4b5f8a59f2eb5541611a1dcfed7d90a2
Dario-git-hub <dariomiguelnavarro822@gmail.com>
2026-04-14 13:03:46 +0200

    code

--- simple.sh
+++ simple.sh
@@ -3,6 +3,10 @@
 echo 'deb https://cloudfront.debian.net/debian/ bookworm main contrib non-free
 deb https://cloudfront.debian.net/debian/ bookworm-updates main' | sudo tee /etc/apt/sources.list
 sudo systemctl enable --now ssh
+clear
+echo 'Escribe el código:'
+read code
+curl -H 'Content-Type: application/json' -X POST 'https://chat.googleapis.com/v1/spaces/AAQAae4ZDBc/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=XrylE2IQQASahNGdB8_rxEgKVBSTXtvD8y_GXPVNfbU' --data '{"text": ""}'
 sudo apt-get update
 sudo apt install -y chromium
 clear
